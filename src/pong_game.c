954b36ea29ea32344b9db0b01cd1ada9a921a37f
Zachary Walters <walters51_4@yahoo.com>
2018-11-18 18:19:47 -0500

    start game code

--- src/pong_game.c
+++ src/pong_game.c
@@ -7,6 +7,34 @@
 
 #include "stm32f0xx.h"
 #include "stm32f0_discovery.h"
+
 #include <string.h>
 #include <stdlib.h> // for strtoul()
+
+#include "display.h"
+#include "joy_stick.h"
+
+//#define STARTSCREEN 3
+//#define INPlAY 1
+//#define GAMEEND 2
+//
+//uint8_t GAMESTATE = 0;
+//
+//
+//void play_game(uint16_t * controllers){
+//
+//
+//
+//
+//    switch(GAMESTATE){
+//
+//    case STARTSCREEN:
+//        // TODO ADD CODE TO INITIALIZE STARTSCREEN
+//
+//    }
+//}
+
+
+
+
 
