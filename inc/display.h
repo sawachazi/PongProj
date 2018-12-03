812035117aa58c16d600ffbcc230eab1577e1c4e
Zachary Walters <walters51_4@yahoo.com>
2018-12-02 20:54:16 -0500

    moved definitions

--- inc/display.h
+++ inc/display.h
@@ -23,6 +23,10 @@
 #define CS2 0x100
 #define CS1 0x200
 
+#define BALL_S 4 // must stay under 8
+#define PADDLE_W 8
+#define PADDLE_L 16
+
 void write_display(int);
 void display_off();
 void display_on();
