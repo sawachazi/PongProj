61afb03274d1dce87d4887a11615de1303e9dcf4
cpkroger <cpkroger@gmail.com>
2018-12-03 22:48:04 -0500

    audio changes

--- src/audio.c
+++ src/audio.c
@@ -26,7 +26,7 @@ void init_pa4(){
  * and the interrupt turns itself off once the threshold is reached.
  */
 void TIM2_IRQHandler(void) {
-	if((TIM3->CR1 & TIM_CR1_CEN)){
+	if((TIM3->CR1 & TIM_CR1_CEN) | (tim2_int == 0)){
 		tim2_int++;
 		TIM3->CR1 &= ~TIM_CR1_CEN;	//Disable TIM3
 		switch(tim2_int){
@@ -127,7 +127,7 @@ void TIM3_IRQHandler(void) {
 
 void tim3_init(void) {
 	RCC->APB1ENR |= RCC_APB1ENR_TIM3EN;
-	TIM3->PSC = 5454-1;
+	TIM3->PSC = 2727-1;
 	TIM3->ARR = 10-1;
 	TIM3->DIER |= TIM_DIER_UIE;
 	NVIC->ISER[0] = 1<<TIM3_IRQn;
@@ -140,6 +140,7 @@ void tim3_init(void) {
 
 void collision(){
 	tim2_int = 8;
+	game_over = 1;
 	TIM2->ARR = 250-1;
 }
 
